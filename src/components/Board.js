9ee58c6f2dbcbe7d038aeb5a6b6a2aa8807ff1ea
saharVac <vcsahar@gmail.com>
2020-12-23 10:56:52 -0500

    Modified tiles state to a dictionary now holding values and a list of the tiles to be solved

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,18 +3,17 @@ import Row from "./Row";
 
 function Board({ isSolving }) {
 
-    console.log('Solving: ', isSolving)
+    // const initialBoard = []
+    // for (let i = 0; i < 9; i++) {
+    //     const row = []
+    //     for (let j = 0; j < 9; j++) {
+    //         row.push(0)
+    //     }
+    //     initialBoard.push(row)
+    // }
 
-    const initialBoard = []
-    for (let i = 0; i < 9; i++) {
-        const row = []
-        for (let j = 0; j < 9; j++) {
-            row.push(0)
-        }
-        initialBoard.push(row)
-    }
-
-    const [tiles, setTiles] = useState([
+    const [tiles, setTiles] = useState({
+        values: [
         [3, 0, 0, 9, 0, 6, 8, 7, 1],
         [7, 1, 0, 0, 0, 0, 0, 0, 5],
         [0, 0, 9, 7, 0, 0, 0, 2, 0],
@@ -24,7 +23,24 @@ function Board({ isSolving }) {
         [0, 7, 0, 0, 0, 5, 3, 0, 0],
         [8, 0, 0, 0, 0, 0, 0, 5, 4],
         [2, 9, 5, 1, 0, 4, 0, 0, 8]
-    ]);
+        ],
+        tilesToSolve: []
+    });
+
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            // examine tile at indeces i, j
+            // add empty tiles to tilesToSolve array
+            if (!tiles.values[i][j]) {
+                setTiles({
+                    ...tiles,
+                    tilesToSolve: [...tiles.tilesToSolve, {row: i, col: j}]
+                })
+            }
+        }
+    } 
+
+    console.log('empty tiles: ', tiles.tilesToSolve)
 
     // TODO: incorporate in actual state -->>
     const state = {
@@ -38,7 +54,11 @@ function Board({ isSolving }) {
 
     const board = []
     for (let i = 0; i < 9; i++) {
-        board.push(<Row rowNum={i} values={tiles[i]} tileClick={tileClick} />)
+        board.push(<Row rowNum={i} values={tiles.values[i]} tileClick={tileClick} />)
+    }
+
+    if (isSolving) {
+        
     }
     
     return (
