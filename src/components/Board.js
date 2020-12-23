6be96a0c028db1ddb0b61e63c3fd2db6aad8126c
saharVac <vcsahar@gmail.com>
2020-12-23 11:43:04 -0500

    Added logic to determine if a value is possible at a given tile

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,9 +11,7 @@ function Board({ isSolving }) {
     //     }
     //     initialBoard.push(row)
     // }
-
-    const [tiles, setTiles] = useState({
-        values: [
+    const values = [
         [3, 0, 0, 9, 0, 6, 8, 7, 1],
         [7, 1, 0, 0, 0, 0, 0, 0, 5],
         [0, 0, 9, 7, 0, 0, 0, 2, 0],
@@ -23,22 +21,22 @@ function Board({ isSolving }) {
         [0, 7, 0, 0, 0, 5, 3, 0, 0],
         [8, 0, 0, 0, 0, 0, 0, 5, 4],
         [2, 9, 5, 1, 0, 4, 0, 0, 8]
-        ],
-        tilesToSolve: []
-    });
-
+    ]
+    const tilesToSolve = []
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
             // examine tile at indeces i, j
             // add empty tiles to tilesToSolve array
-            if (!tiles.values[i][j]) {
-                setTiles({
-                    ...tiles,
-                    tilesToSolve: [...tiles.tilesToSolve, {row: i, col: j}]
-                })
+            if (!values[i][j]) {
+                tilesToSolve.push({row: i, col: j})
             }
         }
-    } 
+    }
+
+    const [tiles, setTiles] = useState({
+        values: values,
+        tilesToSolve: tilesToSolve
+    });
 
     console.log('empty tiles: ', tiles.tilesToSolve)
 
@@ -57,8 +55,48 @@ function Board({ isSolving }) {
         board.push(<Row rowNum={i} values={tiles.values[i]} tileClick={tileClick} />)
     }
 
-    if (isSolving) {
+    const isPossVal = (row, column, value) => {
+        // if the value already exists in the row, return false
+        if (tiles.values[row].includes(value)) {
+            return false
+        }
+        // if the value already exists in the column, return false
+        for (let i = 0; i < 9; i++) {
+            if (tiles.values[i][column] === value) {
+                return false
+            }
+        }
+        // A box holds 3 x 3 tiles
+        const boxRow = Math.floor(row / 3)
+        const boxCol = Math.floor(column / 3)
+        // iterate over rows of box
+        for (let i = boxRow * 3; i < (boxRow * 3) + 3; i++) {
+            // iterate over columns of box
+            for (let j = boxCol * 3; j < (boxCol * 3) + 3; j++) {
+                // if the value already exists in the tile, return false
+                if (tiles.values[i][j] === value) {
+                    console.log('value', value, 'found at row:', i, 'column:', j)
+                    return false
+                }
+            }
+        }
         
+    }
+
+    const liveSolution = () => {
+        tiles.tilesToSolve.forEach(tile => {
+            // iterate over all digits
+            for (let testValue = 1; testValue <= 9; testValue++) {
+                // check if value is possible
+                if (isPossVal(tile.row, tile.col, testValue)) {
+                    
+                }
+            }
+        })
+    }
+
+    if (isSolving) {
+        liveSolution()
     }
     
     return (
