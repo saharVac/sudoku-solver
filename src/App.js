4e06966a75140177f02a0d01da28b9b811f6915b
saharv-av <vcsahar@gmail.com>
2022-01-13 01:45:24 -0500

    added a function that checks if a value is possible before entering it and fixed ability to clear focused cell

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Board from './Components/Board'
 import ModificationSection from './Components/ModificationSection'
 import React, { useState } from 'react';
 
+// TODO: create pop up alert when value not possible instead of using alert()
+
 
 function App() {
 
@@ -43,16 +45,78 @@ function App() {
 
   }
 
+  const isValuePossible = (value) => {
+
+    // value is possible by default
+    let isPoss = true
+    let contradictingCell
+    const row = focusedCell.row
+    const column = focusedCell.column
+
+    // check if value possible for focused cell's row
+    // iterate over row values
+    const rowValues = board[row]
+    rowValues.forEach((cellValue, cellColumn) => {
+      // if value is found in row, value not possible
+      if (parseInt(cellValue) === value) {
+        contradictingCell = {
+          row: row,
+          column: cellColumn
+        }
+        isPoss = false
+      }
+    })
+
+    // check if value possible for focused cell's column
+    // iterate over column
+    let columnValues = [board[0][column], board[1][column], board[2][column], board[3][column], board[4][column], board[5][column], board[6][column], board[7][column], board[8][column]]
+    columnValues.forEach((cellValue, cellRow) => {
+      // if value is found in column, value not possible
+      if (parseInt(cellValue) === value) {
+        contradictingCell = {
+          row: cellRow,
+          column: column
+        }
+        isPoss = false
+      }
+    })
+
+    // check if value possible for focused cell's box
+    const boxRows = (row > 5) ? [6, 7, 8] : (row > 2) ? [3, 4, 5] : [0, 1, 2]
+    const boxColumns = (column > 5) ? [6, 7, 8] : (column > 2) ? [3, 4, 5] : [0, 1, 2]
+    boxRows.forEach(rowToCheck => {
+      boxColumns.forEach(columnToCheck => {
+        // if value is found in column, value not possible
+        if (board[rowToCheck][columnToCheck] === value) {
+          contradictingCell = {
+            row: rowToCheck,
+            column: columnToCheck
+          }
+          isPoss = false
+        }
+      })
+    })
+
+    console.log("contradicting cell:", contradictingCell)
+    return isPoss
+  }
+
   const changeCell = (value) => {
-    let newBoard = board
-    console.log("click")
-    newBoard[focusedCell.row][focusedCell.column] = value
-    setBoard(newBoard)
-    $("#cell-" + focusedCell.row + "-" + focusedCell.column).html(value)
+    // check if possible to input value given the current values in cell's row column and cube
+    if (isValuePossible(value)) {
+      let newBoard = board
+      newBoard[focusedCell.row][focusedCell.column] = value
+      setBoard(newBoard)
+      $("#cell-" + focusedCell.row + "-" + focusedCell.column).html(value)
+    }
   }
 
   const clearValue = () => {
-    console.log("clearing board")
+    console.log("clearing value")
+    let newBoard = board
+    newBoard[focusedCell.row][focusedCell.column] = 0
+    setBoard(newBoard)
+    $("#cell-" + focusedCell.row + "-" + focusedCell.column).html("")
   }
 
   return (
